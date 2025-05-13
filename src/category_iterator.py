57d53c20ae41922cf26a3d6a13934bc712656aa5
Natalya Sorokina <natali83911@gmail.com>
2025-05-13 19:49:42 +0300

    added files

---
+++ src/category_iterator.py
@@ -0,0 +1,43 @@
+# Создайте новый вспомогательный класс, с помощью которого можно перебирать товары одной категории,
+# например в цикле for. Для этого новый класс должен принимать на вход объект класса категории и
+# производить итерацию по товарам, которые хранятся в данной категории. То есть метод выполнения
+# следующего шага итерации должен возвращать очередной товар категории.
+from src.category import Category
+from src.product import Product
+
+
+class CategoryIterator:
+    "Перебирает товары одной категории и возвращает очередной товар категории."
+
+    def __init__(self, category_obj):
+        self.__products = category_obj
+        self.index = 0
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.__products.products_list):
+            product = self.__products.products_list[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
+
+
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    iterator = CategoryIterator(category)
+
+    for product in iterator:
+        print(product)
