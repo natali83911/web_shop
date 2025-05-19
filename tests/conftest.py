976832b89c40cb61a031be61a6e0a13d41e862e8
Natalya Sorokina <natali83911@gmail.com>
2025-05-19 18:59:52 +0300

    modified 2 files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 
 from src.category import Category
+from src.lawn_grass import LawnGrass
 from src.product import Product
+from src.smartphone import Smartphone
 
 
 @pytest.fixture
@@ -42,3 +44,23 @@ def product_category_for_iter(product_unit, product_unit_2) -> Category:
     """Фикстура возвращает категорию с двумя продуктами"""
     products = [product_unit, product_unit_2]
     return Category("Мобильная электроника", "Smartphone", products)
+
+
+@pytest.fixture
+def smartphone1():
+    return Smartphone("Phone1", "Desc1", 1000.0, 2, "A", "Model1", "64GB", "Black")
+
+
+@pytest.fixture
+def smartphone2():
+    return Smartphone("Phone2", "Desc2", 1500.0, 3, "B", "Model2", "128GB", "White")
+
+
+@pytest.fixture
+def grass1():
+    return LawnGrass("Grass1", "Desc1", 10.0, 5, "CountryA", "7 days", "Green")
+
+
+@pytest.fixture
+def grass2():
+    return LawnGrass("Grass2", "Desc2", 15.0, 3, "CountryB", "10 days", "Light Green")
